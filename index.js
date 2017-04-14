07a8baf76579f383772c0df39f5bfde49cf5ba67
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-14 17:57:21 +0100

    Refactor the code to be simpler and clearer + add some comments to clarify few things

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
+// in theory, the following two collections can be extended to support number above 3999
+// add the collection of the roman digits according to the rules from wikipedia
+const romanDigits = ['X', 'IX', 'V', 'IV', 'I'];
+// add the corresponding collection of the arabic numbers
+const arabicDigits = [10, 9, 5, 4, 1];
+
 const convertToRoman = (input) => {
-  const romanDigits = ['X', 'IX', 'V', 'IV', 'I'];
-  const arabicDigits = [10, 9, 5, 4, 1];
-
-  let remaining = input;
-  const result = arabicDigits.reduce((accumulator, value, index) => {
-    while(remaining >= value) {
-      if(remaining - value > -1) {
-        remaining = remaining - value;
-        accumulator += romanDigits[index];
-      }
+  const output = arabicDigits.reduce((romanValue, arabicValue, index) => {
+    // loop until the remaining value is larger or equal to the current arabic number
+    while(input >= arabicValue) {
+      input = input - arabicValue;
+      romanValue = `${romanValue}${romanDigits[index]}`;
     }
-    return accumulator;
-    }, '');
+    return romanValue;
+  }, '');
 
-  return result;
+  return output;
 }
 
 module.exports = {
