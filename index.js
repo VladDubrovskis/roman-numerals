a5bb0753238589dd48df3bfaee5100864e64e5da
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-14 19:40:36 +0100

    Refactor the conversion of roman numbers - go back to initial collections and make the code smart to handle the cases of subtractive notation

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // in theory, the following two collections can be extended to support number above 3999
 // add the collection of the roman digits according to the rules from wikipedia
-const romanDigits = ['X', 'IX', 'VIII', 'VII', 'VI', 'V', 'IV', 'III', 'II', 'I'];
+const romanDigits = ['X', 'IX', 'V', 'IV', 'I'];
 // add the corresponding collection of the arabic numbers
-const arabicDigits = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
+const arabicDigits = [10, 9, 5, 4, 1];
 
 const convertToRoman = (input) => {
   const output = arabicDigits.reduce((romanValue, arabicValue, index) => {
@@ -18,14 +18,19 @@ const convertToRoman = (input) => {
 }
 
 const convertToArabic = (input) => {
-  const output = romanDigits.reduce((arabicValue, romanValue, index) => {
+  const output = romanDigits.reduceRight((arabicValue, romanValue, index) => {
     // create a new regex to match the roman numeral
-    const romanDigitsExpression = new RegExp(`^(${romanValue})+$`, 'g');
+    const romanDigitsExpression = new RegExp(`(${romanValue})+$`, 'g');
     // check if we can find any occurences using the regex created
     const checkOccurences = input.match(romanDigitsExpression);
     if(checkOccurences) {
-      input.replace(romanDigitsExpression, '');
-      arabicValue = checkOccurences.pop().length / romanDigits[index].length * arabicDigits[index]
+      // remove the occurences 
+      input = input.replace(romanDigitsExpression, '');
+      // as we are matching a group of characters, e.g. III will be match as such, we need to work out how many occurences are matched
+      // so we take the matches length, and divide it by the length of the roman value
+      // e.g. III(3) divided by I(1) will mean 3 occurences
+      const occurences = checkOccurences.pop().length / romanDigits[index].length;
+      arabicValue = arabicValue + (occurences * arabicDigits[index])
     }
     return arabicValue;
   }, 0);
