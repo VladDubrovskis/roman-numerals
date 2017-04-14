9508c57ba5576e6de8c07da5bebc30035aff3acf
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-14 10:48:01 +0100

    Extend first test to cover number from 1 to 10

--- test/index.test.js
+++ test/index.test.js
@@ -3,6 +3,17 @@ const assert = require('assert');
 
 describe('Roman numeral converter', () => {
   it('should convert number 1', () => {
+  it('should convert numbers 1 to 10', () => {
     assert.equal(index.convertToRoman(1), 'I');
+    assert.equal(index.convertToRoman(2), 'II');
+    assert.equal(index.convertToRoman(3), 'III');
+    assert.equal(index.convertToRoman(4), 'IV');
+    assert.equal(index.convertToRoman(5), 'V');
+    assert.equal(index.convertToRoman(6), 'VI');
+    assert.equal(index.convertToRoman(7), 'VII');
+    assert.equal(index.convertToRoman(8), 'VIII');
+    assert.equal(index.convertToRoman(9), 'IX');
+    assert.equal(index.convertToRoman(10), 'X');
+  });
   });
 });
