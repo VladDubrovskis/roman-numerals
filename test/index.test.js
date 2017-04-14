763306afc3cda74a2bc5be4ee077fad0ae69e49a
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-14 18:12:58 +0100

    Add a test for converting the roman values back to arabic digits

--- test/index.test.js
+++ test/index.test.js
@@ -2,12 +2,20 @@ const index = require('../index');
 const assert = require('assert');
 
 describe('Roman numeral converter', () => {
-  it('should convert numbers 1 to 10', () => {
+  it('should convert numbers 1 to 10 to roman values', () => {
     const arabic = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     const roman = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
 
     arabic.forEach((input, numberIndex) => {
       assert.equal(index.convertToRoman(input), roman[numberIndex]);
+    });
+  });
+
+  it('should convert numbers I to X to arabic values', () => {
+    const arabic = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    const roman = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+    roman.forEach((input, numberIndex) => {
+      assert.equal(index.convertToArabic(input), arabic[numberIndex]);
     });
   });
 
