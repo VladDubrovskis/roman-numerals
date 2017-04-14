38e6d54378a5ca0e7b4ac3e03b8222f68aac1fa5
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-14 19:06:23 +0100

    Add converting roman numerals back to arabic. Extend the colletion to contain extra numbers to handle edge cases

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 // in theory, the following two collections can be extended to support number above 3999
 // add the collection of the roman digits according to the rules from wikipedia
-const romanDigits = ['X', 'IX', 'V', 'IV', 'I'];
+const romanDigits = ['X', 'IX', 'VIII', 'VII', 'VI', 'V', 'IV', 'III', 'II', 'I'];
 // add the corresponding collection of the arabic numbers
-const arabicDigits = [10, 9, 5, 4, 1];
+const arabicDigits = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1];
 
 const convertToRoman = (input) => {
   const output = arabicDigits.reduce((romanValue, arabicValue, index) => {
@@ -17,6 +17,23 @@ const convertToRoman = (input) => {
   return output;
 }
 
+const convertToArabic = (input) => {
+  const output = romanDigits.reduce((arabicValue, romanValue, index) => {
+    // create a new regex to match the roman numeral
+    const romanDigitsExpression = new RegExp(`^(${romanValue})+$`, 'g');
+    // check if we can find any occurences using the regex created
+    const checkOccurences = input.match(romanDigitsExpression);
+    if(checkOccurences) {
+      input.replace(romanDigitsExpression, '');
+      arabicValue = checkOccurences.pop().length / romanDigits[index].length * arabicDigits[index]
+    }
+    return arabicValue;
+  }, 0);
+
+  return output;
+}
+
 module.exports = {
-  convertToRoman
+  convertToRoman,
+  convertToArabic
 }
