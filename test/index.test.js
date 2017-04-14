cedb392403a5fac63b98f5b629f5c51ed8de5720
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-14 19:40:55 +0100

    Update test to check for numbers between 1 and 20

--- test/index.test.js
+++ test/index.test.js
@@ -2,18 +2,18 @@ const index = require('../index');
 const assert = require('assert');
 
 describe('Roman numeral converter', () => {
-  it('should convert numbers 1 to 10 to roman values', () => {
-    const arabic = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const roman = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+  it('should convert numbers 1 to 20 to roman values', () => {
+    const arabic = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    const roman = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX'];
 
     arabic.forEach((input, numberIndex) => {
       assert.equal(index.convertToRoman(input), roman[numberIndex]);
     });
   });
 
-  it('should convert numbers I to X to arabic values', () => {
-    const arabic = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-    const roman = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X'];
+  it('should convert numbers I to XX to arabic values', () => {
+    const arabic = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
+    const roman = ['I', 'II', 'III', 'IV', 'V', 'VI', 'VII', 'VIII', 'IX', 'X', 'XI', 'XII', 'XIII', 'XIV', 'XV', 'XVI', 'XVII', 'XVIII', 'XIX', 'XX'];
     roman.forEach((input, numberIndex) => {
       assert.equal(index.convertToArabic(input), arabic[numberIndex]);
     });
