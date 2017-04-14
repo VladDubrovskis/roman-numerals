13576910088f9fb693a15c0cadcb443eb07ce68c
Vlad Dubrovskis <vlad@vovka.co.uk>
2017-04-14 11:55:20 +0100

    Refactor the converter to make use of array reducer and by applying the roman digit rules

--- index.js
+++ index.js
@@ -1,38 +1,19 @@
 const convertToRoman = (input) => {
-  switch (input) {
-    case 1:
-      return 'I'
-      break;
-    case 2:
-      return 'II'
-      break;
-    case 3:
-      return 'III'
-      break;
-    case 4:
-      return 'IV'
-      break;
-    case 5:
-      return 'V'
-      break;
-    case 6:
-      return 'VI'
-      break;
-    case 7:
-      return 'VII'
-      break;
-    case 8:
-      return 'VIII'
-      break;
-    case 9:
-      return 'IX'
-      break;
-    case 10:
-      return 'X'
-      break;
-    default:
-      return input;
-  }
+  const romanDigits = ['X', 'IX', 'V', 'IV', 'I'];
+  const arabicDigits = [10, 9, 5, 4, 1];
+
+  let remaining = input;
+  const result = arabicDigits.reduce((accumulator, value, index) => {
+    while(remaining >= value) {
+      if(remaining - value > -1) {
+        remaining = remaining - value;
+        accumulator += romanDigits[index];
+      }
+    }
+    return accumulator;
+    }, '');
+
+  return result;
 }
 
 module.exports = {
